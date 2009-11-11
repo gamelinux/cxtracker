f78794718183b385a1d3986cbe2c9dcc7bf9bfdc
Edward <edward@aruba.linpro.no>
2009-11-11 13:54:30 +0100

    Found an Error - Cant remember how it got there :/ Sorry!

--- src/cxtracker.c
+++ src/cxtracker.c
@@ -193,15 +193,18 @@ void cx_track(struct in6_addr ip_src,uint16_t src_port,struct in6_addr ip_dst,ui
 
    while ( cxt != NULL ) {
       if (af == AF_INET) {
-         if ( cxt->s_ip.s6_addr32[0] == ip_src.s6_addr32[0] && cxt->d_ip.s6_addr32[0] == ip_dst.s6_addr32[0]
-              && cxt->s_port == src_port && cxt->d_port == dst_port ) {
+//         if ( cxt->s_ip.s6_addr32[0] == ip_src.s6_addr32[0] && cxt->d_ip.s6_addr32[0] == ip_dst.s6_addr32[0]
+//              && cxt->s_port == src_port && cxt->d_port == dst_port ) {
+         if ( memcmp(&cxt->s_ip,&ip_src,4) && memcmp(&cxt->d_ip,&ip_dst,4) && cxt->s_port == src_port && cxt->d_port == dst_port ) {
             cxt->s_tcpFlags    |= tcpflags;
             cxt->s_total_bytes += p_bytes;
             cxt->s_total_pkts  += 1;
             cxt->last_pkt_time  = tstamp;
             return;
          }
-         else if ( memcmp(&cxt->s_ip,&ip_src,4) ) {
+//         else if ( cxt->s_ip.s6_addr32[0] == ip_dst.s6_addr32[0] && cxt->d_ip.s6_addr32[0] == ip_src.s6_addr32[0]
+//                   && cxt->d_port == src_port && cxt->s_port == dst_port ) {
+         else if ( memcmp(&cxt->s_ip,&ip_dst,4) && memcmp(&cxt->d_ip,&ip_src,4) && cxt->d_port == src_port && cxt->s_port == dst_port ) {
             cxt->d_tcpFlags    |= tcpflags;
             cxt->d_total_bytes += p_bytes;
             cxt->d_total_pkts  += 1;
