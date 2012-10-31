1ac561ed4cbf10f1dec6b81fed54cf3601f6c8a3
firnsy <firnsy@securixlive.com>
2012-11-01 10:56:56 +1100

    updated: minor version bump.

--- src/cxtracker.h
+++ src/cxtracker.h
@@ -31,7 +31,7 @@
 #endif /* __FreeBSD__ */
 
 /*  D E F I N E S  ************************************************************/
-#define VERSION                       "0.9.8.1"
+#define VERSION                       "0.9.8.2"
 #define TIMEOUT                       45
 #define BUCKET_SIZE                   65536
 #define SNAPLENGTH                    1600
